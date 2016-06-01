76efc5afe9d7cf7a6ff81245edfa8db081adc060
İbrahim AKŞİT <ibrahimaksit@gmail.com>
2016-06-01 10:22:48 +0300

    Assignment: Optional
    Assignment: Optional- Installing Python Screen Shots

---
+++ Week1/Optional.py
@@ -0,0 +1,5 @@
+#Please upload an image of text editor showing your Python code with single print statement (not print “hello word”)
+print "Hi there! This is my first Python Program"
+
+#Please upload an image showing your Python code running in the command line or terminal. Please include both the python command as well as the output from the program.
+Hi there! This is my first Python Program
